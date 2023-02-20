85a3d92e08af0ad18f2dbe642942d25d53f92dc5
TimeWarper2000 <56519868+TimeWarper2000@users.noreply.github.com>
2023-02-20 13:44:58 -0800

    working join room stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,52 @@
-import "./App.css";
+import './App.css';
 import io from 'socket.io-client';
-const socket = io.connect("https://localhost:3001");
+import { useEffect, useState } from "react";
+const socket = io.connect("http://localhost:3001");
+
 
 function App() {
+  const [room, setRoom] = useState("");
 
-  const sendMessage = () => {
-    //socket.emit("send_message", { message: "hello" });
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("")
+  
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
   }
+  
+  const sendMessage = () => {
+    socket.emit("send_message", { message, room });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket])
+
   return (
     <div className="App">
-      <input placeholder='Message...'/>
+      <input
+      placeholder='Message...'
+      onChange={(event) => {
+        setRoom(event.target.value);
+      }}
+      />
+      <button onClick={joinRoom}> Join Room</button>
+      <input
+      placeholder='Message...'
+      onChange={(event) => {
+        setMessage(event.target.value);
+      }}
+      />
       <button onClick={sendMessage}> Send Message</button>
+    <h1>Message:</h1>
+    {messageReceived}
     </div>
-  ); 
-} 
+  );
+}
+
 
 export default App;
