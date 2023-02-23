4159f3d7d44cfe5d5a5c4fa3a2b179702df11d23
TimeWarper2000 <56519868+TimeWarper2000@users.noreply.github.com>
2023-02-23 09:39:18 -0800

    help

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import { useEffect, useState } from "react";
 import "./App.css";
 const socket = io.connect("http://localhost:3001");
 
+const playerType = 0;
+
+
 function App() {
   const [room, setRoom] = useState("");
 
@@ -19,10 +22,10 @@ function App() {
     }
   }
 
-  
+  //* duplicated in lower function
   const sendMessage = (btnId, pId) => {
     socket.emit("take_turn", { btnId, pId, room });
-  };
+  }; //*
 
   useEffect(() => {
     socket.on("update_UI", (data) => {
@@ -38,6 +41,7 @@ function App() {
   var ammo1 = 0;
   var ammo2 = 0;
 
+
   return ( //TODO --------------------------------------------- ALL HTML HERE
     <div className="App">
       <br></br>
@@ -51,31 +55,87 @@ function App() {
       <button onClick={joinRoom}> Join Room</button>
 
     <div className="container">
-      <div className="left">
-        <h2>Player 1</h2>  
-        {/* //! Change to say what their userid is */}
-        <p id="am1">0</p>
-        <img src={require('./images/p1.png')} alt="Player 1" className="stickman"/>
-        <div className="button-container">
-          <button className="button" onClick={() => {sendMessage(0, 0)}}>Reload</button> 
-          <button className="button" onClick={() => {sendMessage(1, 0)}}>Shoot</button>
-          <button className="button" onClick={() => {sendMessage(2, 0)}}>Block</button>
-        </div>
-      </div>
-      <div className="right">
-        <h2>Player 2</h2>
-        <p id="am2">0</p>
-        <img src={require('./images/p21.png')} alt="Player 2" id="p2" className="stickman"/>
-        <div className="button-container">
-          <button className="button" onClick={() => {sendMessage(0, 1)}}>Reload</button>
-          <button className="button" onClick={() => {sendMessage(1, 1)}}>Shoot</button>
-          <button className="button" onClick={() => {sendMessage(2, 1)}}>Block</button>
-        </div>
-      </div>
+      {/* insert code here  (this <buttons/> )*/}
+    <div className="left">
+            <h2>Player 1</h2>  
+            {/* //! Change to say what their userid is */}
+            <p id="am1">0</p>
+            <img src={require('./images/p1.png')} alt="Player 1" className="stickman"/>
+            <div className="button-container">
+              <button className="button" onClick={() => {sendMessage(0, 0)}}>Reload</button>
+              <button className="button" onClick={() => {sendMessage(1, 0)}}>Shoot</button>
+              <button className="button" onClick={() => {sendMessage(2, 0)}}>Block</button>
+            </div>
+          </div>
+          <div className="right">
+            <h2>Player 2</h2>
+            <p id="am2">0</p>
+            <img src={require('./images/p21.png')} alt="Player 2" id="p2" className="stickman"/>
+            {/*//TODO pretend we are user 0 and try to make the if code. Use google */}
+            <div className="button-container">
+              <button className="button" onClick={() => {sendMessage(0, 1)}}>Reload</button>
+              <button className="button" onClick={() => {sendMessage(1, 1)}}>Shoot</button>
+              <button className="button" onClick={() => {sendMessage(2, 1)}}>Block</button>
+            </div>
+
+  </div>
+
+      {/* end of code insert */}
     </div>
     </div>
   );
 }  //! -------------------------------------------------------------- END
 
+const buttons = () => {
+  // const sendMessage = (btnId, pId) => {
+  //   socket.emit("take_turn", { btnId, pId, room });
+  // };
+
+  if (playerType = 0) {
+  //   return(
+  //     <div>
+  //     <div className="left">
+  //       <h2>Player 1</h2>
+  //       {/* //! Change to say what their userid is */}
+  //       <p id="am1">0</p>
+  //       <img src={require('./images/p1.png')} alt="Player 1" className="stickman"/>
+  //       <div className="button-container">
+  //         <button className="button" onClick={() => {sendMessage(0, 0)}}>Reload</button>
+  //         <button className="button" onClick={() => {sendMessage(1, 0)}}>Shoot</button>
+  //         <button className="button" onClick={() => {sendMessage(2, 0)}}>Block</button>
+  //       </div>
+  //     </div>
+  //     <div className="right">
+  //       <h2>Player 2</h2>
+  //       <p id="am2">0</p>
+  //       <img src={require('./images/p21.png')} alt="Player 2" id="p2" className="stickman"/>
+
+  //     </div>
+  //     </div>
+  //   )
+  // } else {
+  //   <div>
+  //     <div className="left">
+  //       <h2>Player 1</h2>  
+  //       {/* //! Change to say what their userid is */}
+  //       <p id="am1">0</p>
+  //       <img src={require('./images/p1.png')} alt="Player 1" className="stickman"/>
+  //     </div>
+  //     <div className="right">
+  //       <h2>Player 2</h2>
+  //       <p id="am2">0</p>
+  //       <img src={require('./images/p21.png')} alt="Player 2" id="p2" className="stickman"/>
+  //       {/*//TODO pretend we are user 0 and try to make the if code. Use google */}
+  //       <div className="button-container">
+  //         <button className="button" onClick={() => {sendMessage(0, 1)}}>Reload</button>
+  //         <button className="button" onClick={() => {sendMessage(1, 1)}}>Shoot</button>
+  //         <button className="button" onClick={() => {sendMessage(2, 1)}}>Block</button>
+  //       </div>
+
+  //     </div>
+  //     </div>
+  }
+}
 
 export default App;
+
--- server/index.js
+++ server/index.js
@@ -19,6 +19,10 @@ const io = new Server(server, {
 });
 
 var playerTurn = 0;
+var ammo1 = 0;
+var ammo2 = 0;
+var p1Choice = 0;
+var p2Choice = 0;
 
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
@@ -33,14 +37,52 @@ io.on("connection", (socket) => {
     });
 
     socket.on("take_turn", (data) => { //? Maybe change "send message" to be like take turn (do on  both sides)
+        console.log(data);
         if (playerTurn == 0) {
-            console.log(data);
-            if (data === "Reload") {
-                
+            p1Choice = data.btnId;
+
+            if (data.btnId == 0) {
+                console.log("Player 1 Reloaded!!");
+                ammo1++;
+            } else if (data.btnId == 1) {
+
+            } else if (data.btnId == 2) {
+
+            } else {
+                console.log("INVALID button ID. (not 0, 1, or 2). See line 49, or wherever this is.");
             }
             playerTurn++;
-        } else {//TODO -send back ammo(both peeps) -if alive -what button both peeps had
+        } else {
+            p2Choice = data.btnId;
+            if (data.btnId == 0) {
+                console.log("Player 2 Reloaded!!  (Player 2)");
+                ammo2++;
+            } else if (data.btnId == 1) {
+
+            } else if (data.btnId == 2) {
+
+            } else {
+                console.log("INVALID button ID. (not 0, 1, or 2). See line 49, or wherever this is.");
+            }
             playerTurn = 0;
+
+            if (p1Choice == 0 && p2Choice == 1) {
+                //Game ends, p2 wins
+            } else if (p1Choice == 1 && p2Choice == 0) {
+                //game ends, p1 wins
+            } else if (p1Choice == 0 && p2Choice == 0) {
+                // ammo increases
+            } else if (p1Choice == 1 && p2Choice == 1) {
+                // ammo decreases. game continues.
+            } else if (p1Choice == 2 && p2Choice == 2) {
+                // do nothing
+            } else if (p1Choice == 1 && p2Choice == 2) {
+                //p1 ammo decrease
+            } else if (p1Choice == 2 && p2Choice == 1) {
+                //p2 ammo decrease
+            }
+            
+            //TODO analyze state here: send back ammo(both peeps) -if alive -what button both peeps had pressed
             socket.to(data.room).emit("update_UI", data); //?  change "receive_message" to turn over and update UI (on both sides)
         }
     });
